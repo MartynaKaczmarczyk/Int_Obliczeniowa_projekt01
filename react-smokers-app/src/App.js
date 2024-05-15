27cf4bdad03090ae9d48178f2ecc107881a33e01
=Martyna Kaczmarczyk <=marti.kaczmarczyk@wp.pl>
2024-05-16 01:17:19 +0200

    add images generating from frontend side

--- react-smokers-app/src/App.js
+++ react-smokers-app/src/App.js
@@ -36,6 +36,38 @@ function App() {
         }
     };
 
+    /////////////////GENERATOR PART
+    const [imageSrc, setImageSrc] = useState('');
+
+    const handleButtonClick = () => {
+      fetch('http://127.0.0.1:5000/get_image_data')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.blob();
+        })
+
+        .then(a => {
+              fetch('http://127.0.0.1:5000/send_image')
+              .then(response => {
+                if (!response.ok) {
+                throw new Error('Network response was not ok 2');
+                    } else {
+                      return response.blob();
+                    }
+                    })
+               .then(blob => {
+                // Utwórz adres URL dla pobranego obrazu
+                const imageUrl = URL.createObjectURL(blob);
+                setImageSrc(imageUrl);
+                console.log(imageUrl)
+              })
+      })
+        .catch(error => console.error('There was a problem with the fetch operation:', error));
+    };
+  
+    
     return (
         <div className="App">
             <h1>Image Classification</h1>
@@ -52,6 +84,13 @@ function App() {
                 </div>
             )}
             {prediction && <h2>Prediction: {prediction}</h2>}
+            <div>
+      <div>
+      <h1>Generator</h1>
+      <button onClick={handleButtonClick}>Get Image</button>
+      {imageSrc && <img src={imageSrc} alt="Image" />}
+      </div>
+    </div>
         </div>
     );
 }
